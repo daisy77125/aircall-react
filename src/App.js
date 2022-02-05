a94d5adcc247fa5d3c708a7cb61f5e28de53da74
daisy77125 <32116413+daisy77125@users.noreply.github.com>
2022-02-05 17:37:16 -0500

    basic cleanup; import custom css sheets, index.html and App.js; set up react routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import PaginationBar from "./components/PaginationBar.js";
+import Header from "./components/Header.js";
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Header />
+        <div className="container-view">
+          <Routes></Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
